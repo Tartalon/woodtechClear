bae4fc174ceda27392367b7d375924339730dccb
Vladimir <50200804+Tartalon@users.noreply.github.com>
2022-04-06 16:51:10 +0300

    slider whithout adaptive

--- js/common.js
+++ js/common.js
@@ -1,4 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
+  $(".home-slider__heading").each(function () {
+    let text = $(this).text().split(" ");
+    let first = text.shift();
+    $(this).html(`${first} <br/><span>${text.join(" ")}</span>`);
+  });
+
   // Menu
 
   $(".menu-toggle").click(function () {
@@ -26,5 +32,27 @@ document.addEventListener("DOMContentLoaded", () => {
     speed: 800,
     effect: "fade",
     centeredSlides: true,
+    pagination: {
+      el: ".home-slider__pagination",
+      type: "custom",
+      renderCustom: function (swiper, current, total) {
+        let indTotal = total >= 10 ? total : `0${total}`;
+        let indCurrent = current >= 10 ? current : `0${current}`;
+        return `<b>${indCurrent}</b><span></span> ${indTotal}`;
+      },
+    },
+    scrollbar: {
+      el: ".home-slider__scrollbar",
+      draggable: true,
+    },
+    navigation: {
+      prevEl: ".home-slider__prev",
+      nextEl: ".home-slider__next",
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+    runCallbacksOnInit: true,
   });
 });
